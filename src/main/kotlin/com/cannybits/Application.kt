f6958efa6c204b3fdcd6a5e22a46c781c82ee5bf
Nyandago <paulmutator@gmail.com>
2022-09-24 11:42:56 +0300

    Initial commit

---
+++ src/main/kotlin/com/cannybits/Application.kt
@@ -0,0 +1,45 @@
+package com.cannybits
+
+import io.ktor.server.application.*
+import com.cannybits.plugins.*
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    //configureRouting()
+//    routing {
+//        get("/") {
+//            call.respondText("Welcome to Canny ville!!")
+//        }
+//    }
+
+    install(Routing){
+        get("/"){
+            call.respondText("Twenzetu App")
+        }
+
+        get("/users/{username}"){
+            val username = call.parameters["username"]
+            val header = call.request.headers["Connection"]
+
+            if (username == "Admin"){
+                call.response.header(name = "CustomHeader","Admin")
+                call.respond(message="Hello Admin", status = HttpStatusCode.OK)
+            }
+
+            call.respondText("username is $username with header $header")
+        }
+
+        get("/user"){
+            val name = call.request.queryParameters["name"]
+            val age = call.request.queryParameters["age"]
+
+            call.respondText("My name is $name and I am $age years old")
+        }
+    }
+}
